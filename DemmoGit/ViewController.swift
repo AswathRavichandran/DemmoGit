b38612014d5cd805c67eb1fe9dbf6e27056e6a8c
Aswath RA <aswath@dellainfotech.com>
2022-07-14 15:50:56 +0530

    Initial Commit

---
+++ DemmoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemmoGit
+//
+//  Created by Aswath Ravichandran on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
